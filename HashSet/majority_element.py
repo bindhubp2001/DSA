ff708955c9311c02e73a66410e98f1079265831c
bindhubp2001 <139950890+bindhubp2001@users.noreply.github.com>
2024-09-17 13:12:07 +0530

    Add files via upload

---
+++ HashSet/majority_element.py
@@ -0,0 +1,30 @@
+
+# Given an array nums of size n, return the majority element.
+
+# The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+ 
+
+# Example 1:
+
+# Input: nums = [3,2,3]
+# Output: 3
+# Example 2:
+
+# Input: nums = [2,2,1,1,1,2,2]
+# Output: 2
+
+
+class Solution(object):
+    def majorityElement(self, nums):
+        count = {}
+
+        for item in nums:
+            if item in count:
+                count[item] += 1
+            else:
+                count[item] = 1
+        return max(count, key=count.get)
+    
+object = Solution()
+print(object.majorityElement([3,2,3])) # 3
